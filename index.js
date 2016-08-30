114015a87b2549ac14b8c49008509d8ed33ce364
Terje Andersen <terje.andersen@di.no>
2016-08-30 18:38:53 +0200

    Added npm script and engine

---
+++ index.js
@@ -0,0 +1,38 @@
+const SlackBot = require('slackbots');
+
+const bot = new SlackBot({
+  token: 'xoxb-74497822241-fZ8BGUutiY8Pxy5Li4l2dyAr',
+  name: 'fipp'
+})
+
+const compliments = [
+  'You are fantastic!',
+  'You are awesome!',
+  'Your smile is contagious!',
+  'You are an inspiration!'
+];
+
+bot.on('start', () => {
+  bot.postMessageToChannel('general', 'Hello!');
+  bot.postMessageToUser('terje.andersen', 'I am online!');
+})
+
+let currentCompliment = 0;
+
+bot.on('message', function(data) {
+
+  const pattern = /[Cc]ompliment <@(\w+)>/
+  if (data.text && data.text.match(pattern)) {
+    const user = data.text.match(pattern)[1];
+    if (user) {
+      bot.getUserById(user).then(({ name }) => {
+        bot.postMessageToUser(name, compliments[currentCompliment]);
+        currentCompliment = (currentCompliment + 1) % compliments.length;
+      });
+    }
+  }
+
+  if (data.text === 'ping') {
+    bot.postMessage(data.channel, 'pong');
+  }
+})
